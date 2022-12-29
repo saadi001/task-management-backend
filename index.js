e7f84b7a18e08a27344fea56bb4154bb331c9df8
Sheikh sadi <sheikhsadi1620@gmail.com>
2022-12-29 16:28:07 +0600

    delete route added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const port = process.env.PORT || 5000;
@@ -38,6 +38,17 @@ async function run(){
           app.post('/tasks', async(req, res)=>{
                const query = req.body;
                const result = await tasksCollection.insertOne(query);
+               res.send(result)
+          })
+
+          app.put('/myTask/:id', async(req, res)=>{
+               
+          })
+
+          app.delete('/myTask/:id', async(req, res)=>{
+               const id = req.params.id;
+               const filter = {_id: ObjectId(id)}
+               const result = await tasksCollection.deleteOne(filter)
                res.send(result)
           })
 
